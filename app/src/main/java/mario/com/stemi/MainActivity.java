0959c546fc732ae5096fbd4b2166f516d793d25d
Mario Loncar <loncar.mario1@gmail.com>
2016-03-27 18:04:59 +0200

    Joysticks and buttons

---
+++ app/src/main/java/mario/com/stemi/MainActivity.java
@@ -0,0 +1,79 @@
+package mario.com.stemi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton ibMovement, ibRotation, ibOrientation, ibHeight, ibSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //   Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_main);
+
+        ibMovement = (ImageButton) findViewById(R.id.ibMovement);
+        ibRotation = (ImageButton) findViewById(R.id.ibRotation);
+        ibOrientation = (ImageButton) findViewById(R.id.ibOrientation);
+        ibHeight = (ImageButton) findViewById(R.id.ibHeight);
+        ibSettings = (ImageButton) findViewById(R.id.ibSettings);
+
+        ibMovement.setSelected(true);
+
+
+    }
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.ibMovement:
+                ibMovement.setSelected(true);
+                ibRotation.setSelected(false);
+                ibOrientation.setSelected(false);
+
+                break;
+            case R.id.ibRotation:
+                ibRotation.setSelected(true);
+                ibMovement.setSelected(false);
+                ibOrientation.setSelected(false);
+
+                break;
+            case R.id.ibOrientation:
+                ibOrientation.setSelected(true);
+                ibMovement.setSelected(false);
+                ibRotation.setSelected(false);
+
+                break;
+            case R.id.ibHeight:
+
+                break;
+            case R.id.ibSettings:
+
+                break;
+            default:
+                System.out.println("Error!");
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+}
