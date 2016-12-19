399ac529fa69b1bcc1b0b4119e4c86a75a3846b6
Mario Loncar <loncar.mario1@gmail.com>
2016-12-19 22:05:56 +0100

    minor bug fix

--- app/src/main/java/com/stemi/STEMIHexapod/MainActivity.java
+++ app/src/main/java/com/stemi/STEMIHexapod/MainActivity.java
@@ -59,7 +59,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         ibStandby = (ImageButton) findViewById(R.id.ibStandby);
         vOverlay = findViewById(R.id.vOverlay);
         ibMovement = (ImageButton) findViewById(R.id.ibMovement);
